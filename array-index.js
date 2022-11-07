6a575471590103a55d577b665e9a478416d5c154
amirulislam26 <26techamirulislam@gmail.com>
2022-11-07 05:30:46 -0800

    added aprovlem practice solving array index js solved

---
+++ array-index.js
@@ -0,0 +1,14 @@
+var books = ['motu', 'patlu', 'nat', 'boltu', 'gattu', 'battu'];
+var botuIndex = books.indexOf('boltu');
+console.log(botuIndex);
+var nambars = [44, 98, 65, 75, 52, 62];
+var index = nambars.indexOf(152);
+console.log(index);
+
+
+var secontIndex = books[0];
+console.log(secontIndex);
+console.log(nambars);
+nambars[1] = 95;
+nambars[4] = 21;
+console.log(nambars)
