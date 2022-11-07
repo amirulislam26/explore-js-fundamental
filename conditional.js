c904e2990935126f205d34e6c05126dbab7845f9
amirulislam26 <26techamirulislam@gmail.com>
2022-11-07 05:59:11 -0800

    added a conditional provlem solving

---
+++ conditional.js
@@ -0,0 +1,74 @@
+var isFoodReady = false;
+// jodi food ready hoy taile ami khabo
+// if food is ready i will eat
+// if food is read == true
+// if (condition) {
+// 
+// }
+// if (isFoodReady == true) {
+//     console.log('mama!!! i will eat naw')
+// }
+
+
+var iphonePrice = 70000;
+var myBuject = 10000;
+// if (iphonePrice < myBuject) {
+//     console.log('my favorite iphone')
+// }
+// if (iphonePrice > myBuject) {
+//     console.log('my favorite iphone')
+// }
+
+
+var tShartprice = 700;
+var myMoney = 180;
+////////////////////////////
+if (tShartprice > myMoney) {
+    console.log('yes!! i am verry happyu 1')
+}
+else {
+    console.log('No boye to my money 1')
+}
+///////////////////////////////////
+if (tShartprice > myMoney) {
+    console.log('yes!! i am verry happyu 2')
+}
+else {
+    console.log('No boye to my money 2')
+}
+////////////////////////////////////////
+if (tShartprice < myMoney) {
+    console.log('yes!! i am verry happyu 3')
+}
+else {
+    console.log('No boye to my money 3')
+}
+//////////////////////////////////////////
+if (tShartprice >= myMoney) {
+    console.log('yes!! i am verry happyu 4')
+}
+else {
+    console.log('No boye to my money 4')
+}
+//////////////////////////////////
+if (tShartprice <= myMoney) {
+    console.log('yes!! i am verry happyu 5')
+}
+else {
+    console.log('No boye to my money 5')
+}
+///////////////////////////////////////
+if (tShartprice === myMoney) {
+    console.log('yes!! i am verry happyu 6')
+}
+else {
+    console.log('No boye to my money 6')
+}
+/////////////////////////////////////
+if (tShartprice !== myMoney) {
+    console.log('yes!! i am verry happyu 7 ')
+}
+else {
+    console.log('No boye to my money 7')
+}
+/////////////////////////////////////////////////
