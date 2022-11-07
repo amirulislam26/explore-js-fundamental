ee1c672439b0ecf70168597ef28399e04148c52c
amirulislam26 <26techamirulislam@gmail.com>
2022-11-07 05:48:32 -0800

    added a problem solving

---
+++ array.js
@@ -0,0 +1,9 @@
+var friendsAge = [11, 13, 17];
+var muriChanacurFee = [24, 15, 125, 30];
+var nayikas = ['opu', "mahi", 'sabnu', "porimoni"];
+var oldNumbers = [1, 3, 5, 7, 9,];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(nayikas.length);
+var vowelsCount = vowels.length;
+console.log(vowelsCount);
