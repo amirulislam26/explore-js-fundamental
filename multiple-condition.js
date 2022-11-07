081560a7a40d27d93478f89776c6d4bbe2d8e407
amirulislam26 <26techamirulislam@gmail.com>
2022-11-07 06:07:46 -0800

    added a new provlem solving multiple condition

---
+++ multiple-condition.js
@@ -0,0 +1,32 @@
+var godJob = false;
+var moneySaved = 50000;
+var hasFlats = false;
+var hasHouse = true;
+
+// if (godJob == true && moneySaved > 200000) {
+//     console.log('colo life setel kori')
+// }
+// else {
+//     console.log('tor kopale biya nai')
+// }
+
+
+// if (godJob == true && moneySaved > 200000 && hasFlats == true) {
+//     console.log('colo life setel kori')
+// }
+// else {
+//     console.log('tor kopale biya nai')
+// }
+
+// if (godJob == true || moneySaved > 200000) {
+//     console.log('colo life setel kori')
+// }
+// else {
+//     console.log('tor kopale biya nai')
+// }
+if ((godJob == true && moneySaved > 200000) || hasHouse == true) {
+    console.log('colo life setel kori onk valo kicu hobe ')
+}
+else {
+    console.log('tor kopale biya nai ata ami valo kore jani')
+}
